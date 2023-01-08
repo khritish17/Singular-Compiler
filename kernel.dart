020e1cdcf646b8100d0325808b7473ee56a4992b
Khritish Kumar Behera <khritish.official@gmail.com>
2023-01-08 22:01:56 +0530

    first commit | nothing special

---
+++ kernel.dart
@@ -0,0 +1,19 @@
+
+
+void main(List<String> args) {
+  print("Hello");
+  conversion("Hi this is Khritish j\u0302");
+}
+
+String conversion(input_){
+
+  String output_ = "";
+  var words = input_.split("");
+  for (var i = 0; i< words.length; i++){
+    String current_word = words[i];
+    // now check each word 
+  }
+  // print(words);
+  return output_ ;
+
+}
